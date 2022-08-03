500dca869abf574d3c19b0e307a8d8e40b116f59
ChenYuan <cy476571@gmail.com>
2022-08-03 18:20:00 +0800

    add: add more menus btns to sidebar;

--- src/renderer/components/Application/index.tsx
+++ src/renderer/components/Application/index.tsx
@@ -3,6 +3,7 @@
  */
 import React from "react";
 import { postMessage } from "../../utils/index";
+import Divider from "@material-ui/core/Divider";
 
 interface ApplicationProps {
   app: any;
@@ -16,21 +17,76 @@ const ApplicationComp: React.FC<ApplicationProps> = ({ app }) => {
     });
   }
 
+  function openTerminal() {
+    postMessage({
+      type: "openTerminal",
+      data: {},
+    });
+  }
+
+  function portForward() {
+    postMessage({
+      type: "portForward",
+      data: {},
+    });
+  }
+
+  function remoteDebug() {
+    postMessage({
+      type: "remoteDebug",
+      data: {},
+    });
+  }
+
   return (
     <div className="forkmain-app">
-      <button title="Reconnect to remote workspace" onClick={reconnect}>
-        Rerun
-      </button>
+      <div style={{ display: "flex", paddingTop: 10, paddingBottom: 10 }}>
+        <button
+          style={{
+            width: 100,
+            marginLeft: 20,
+          }}
+          title="Reconnect to remote workspace"
+          onClick={reconnect}
+        >
+          Run
+        </button>
+        <button
+          style={{
+            width: 100,
+            marginLeft: 20,
+          }}
+          title="Debug the service with remote debugger"
+          onClick={remoteDebug}
+        >
+          Debug
+        </button>
+      </div>
+      <Divider />
+      <div style={{ display: "flex", paddingTop: 10, paddingBottom: 10 }}>
+        <button
+          style={{
+            width: 100,
+            marginLeft: 20,
+          }}
+          title="Open another remote terminal"
+          onClick={openTerminal}
+        >
+          New Terminal
+        </button>
+        <button
+          style={{
+            width: 100,
+            marginLeft: 20,
+          }}
+          title="Port Forward"
+          onClick={portForward}
+        >
+          Port Forward
+        </button>
+      </div>
     </div>
   );
-
-  // return (
-  //   <div className="forkmain-app">
-  //     <button title="Manage Your Application" onClick={manageApp}>
-  //       Manage Application
-  //     </button>
-  //   </div>
-  // );
 };
 
 export default ApplicationComp;
